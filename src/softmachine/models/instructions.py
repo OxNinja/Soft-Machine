43f9574d8b8b4c166723d08578a3bd1faf8f367d
OxNinja <57400445+OxNinja@users.noreply.github.com>
2023-12-10 19:03:52 +0100

    added sub instruction

--- src/softmachine/models/instructions.py
+++ src/softmachine/models/instructions.py
@@ -58,7 +58,31 @@ def add(vm, opcode):
     vm.regs.set(target, total)
 
 def sub(vm, opcode):
-    pass
+    # first arg is always a register
+    target = opcode >> (vm.opcode_size - 2) * 4 & 0xf
+    if target < 0 or target >= len(vm.regs.regs):
+        print(f"Unexpected value for target: {target}. Must be between 0 (included) and {len(vm.regs.regs)} (excluded).")
+        exit(1)
+
+    # second arg can be either a register, or a value
+    is_reg = opcode >> (vm.opcode_size - 3) * 4 & 0xf
+
+    if is_reg == 0:
+        value = opcode & (16 ** (vm.opcode_size - 3) - 1)
+    elif is_reg == 1:
+        index = opcode >> (vm.opcode_size - 4) * 4 & 0xf
+        if index < 0 or index >= len(vm.regs.regs):
+            print(f"Unexpected value for index: {index}. Must be between 0 (included) and {len(vm.regs.regs)} (excluded).")
+            exit(1)
+        
+        value = vm.regs.get(index)
+
+    elif is_reg != 0 or is_reg != 1:
+        print(f"Unexpected value for is_reg: {is_reg}. Must be either 0 or 1.")
+        exit(1)
+
+    total = vm.regs.get(target) - value
+    vm.regs.set(target, total)
 
 def cmp(vm, opcode):
     pass
